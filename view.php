5da7813ce673303b96e9a4a39aaafcc91aad7cfb
manyusialphonce <manyusialphonce2002@gmail.com>
2026-01-18 22:28:50 +0300

    Format PHP code for displaying contacts list

--- view.php
+++ view.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect("localhost","root","","manyusitech_contacts");
 $result = mysqli_query($conn,"SELECT * FROM whatsapp_contacts");
 echo "<h2>Contacts List</h2>";
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="style.css">
 echo "<table border='1'><tr><th>Name</th><th>Phone</th><th>Group</th><th>WhatsApp Link</th></tr>";
 while($row = mysqli_fetch_assoc($result)){
     echo "<tr>
@@ -13,4 +13,5 @@ while($row = mysqli_fetch_assoc($result)){
     </tr>";
 }
 echo "</table>";
+
 ?>
